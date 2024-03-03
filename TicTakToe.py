b8d7c74729309fbc09cfc71d1b930bbaf2f84df5
prajwalgunjal <prajwalgunjal8@gmail.com>
2024-03-03 12:55:52 +0530

    Players onboarded

--- TicTakToe.py
+++ TicTakToe.py
@@ -12,7 +12,14 @@ def printBoard():
 if __name__ == "__main__":
     xAxis = [0,0,0,0,0,0,0,0]
     yAxis = [0,0,0,0,0,0,0,0]
-
+    turn = 1 # 1 for X and 0 for O
     print("Welcome to TicTakToe Game")
-    #while True:
-    printBoard()
+    p1 = input("Enter the name of the player 1 : you will get X : ")
+    p2 = input("Enter the name of the player 2 : You will get O : ")
+    while True:
+        printBoard()
+        if turn == 1:
+            print(f"{p1}'s Chance")
+        else:
+            print(f"{p2}'s Chance")
+        break
