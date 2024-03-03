86d09b35ea4c3a1494db542cc230594ea68f755f
prajwalgunjal <prajwalgunjal8@gmail.com>
2024-03-03 12:49:02 +0530

    Board printing doen

---
+++ TicTakToe.py
@@ -0,0 +1,18 @@
+
+def printBoard():
+    print(f" 0 | 1 | 2 ")
+    print("---|---|---")
+    print(f" 3 | 4 | 5 ")
+    print("---|---|---")
+    print(f" 6 | 7 | 8 ")
+
+
+
+
+if __name__ == "__main__":
+    xAxis = [0,0,0,0,0,0,0,0]
+    yAxis = [0,0,0,0,0,0,0,0]
+
+    print("Welcome to TicTakToe Game")
+    #while True:
+    printBoard()
